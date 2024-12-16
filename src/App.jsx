0465e28b81a0d2f44de58af0d36b3c41f4b9a0c0
mtech23 <frontendmarcell@gmail.com>
2024-12-17 02:18:19 +0500

    usama-dev

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import { useState } from 'react'
 
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-
 import 'animate.css';
 import './App.css'
 import "./assets/css/media.css"
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 import AppRouter from './router/Router'
 
