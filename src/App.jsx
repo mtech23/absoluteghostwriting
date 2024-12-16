767a7abf09781ff414e4886fc695c31df54b7b31
mtech23 <frontendmarcell@gmail.com>
2024-12-17 02:06:07 +0500

    maaz-pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 import 'animate.css';
 import './App.css'
 import "./assets/css/media.css"
